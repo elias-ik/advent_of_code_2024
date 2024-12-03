0ce25f1fc6b77652ca21c4f005436bc3db9f1669
Elias <info@elias.dev>
2024-12-03 21:37:56 +0100

    Add Day 1 Part 2

--- advent_of_code_2024/Program.cs
+++ advent_of_code_2024/Program.cs
@@ -10,4 +10,10 @@ var d1 = new Day1()
 var d1Solution = d1.Solve();
 Console.WriteLine($"Day 1 solution: {d1Solution}");
 
+var d1Part2 = new Day1_Part2()
+{
+    InputFile = "./Day1/input.txt"
+};
+var d1Part2Solution = d1Part2.Solve();
+Console.WriteLine($"Day 1 Part 2 solution: {d1Part2Solution}");
 
